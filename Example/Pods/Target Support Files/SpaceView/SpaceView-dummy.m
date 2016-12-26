fa5faa6e36a494662bbac65695e8c2833aa04017
Xopoko <horokko@mail.ru>
2016-12-26 16:53:46 +0300

    fix rep

--- Example/Pods/Target Support Files/SpaceView/SpaceView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SpaceView : NSObject
-@end
-@implementation PodsDummy_SpaceView
-@end
--- Example/build/Pods.build/Debug-iphoneos/SpaceView.build/DerivedSources/SpaceView_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char SpaceViewVersionString[];
- extern const double SpaceViewVersionNumber;
-
- const unsigned char SpaceViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SpaceView  PROJECT:Pods-1" "\n";
- const double SpaceViewVersionNumber __attribute__ ((used)) = (double)1.;
